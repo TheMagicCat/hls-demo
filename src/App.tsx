329ce4724a78ff24eb06abaa17fdd9f2fa2e4105
Matt Wilson <633478+TheMagicCat@users.noreply.github.com>
2023-05-03 14:43:29 -0500

    playing HLS video

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,42 @@
+import Hls from 'hls.js'
+import { useEffect, useRef } from 'react'
+// import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+// import ffmpeg_core_path from '@ffmpeg/core/dist/ffmpeg-core.js?url'
+// const ffmpeg = createFFmpeg({ corePath: ffmpeg_core_path, log: true })
+
+// ;(async () => {
+//   await ffmpeg.load()
+//   console.log(ffmpeg)
+// })()
+
+const source = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'
+
+
 export const App = ({ msg }: { msg?: string }) => {
-  return <p>{msg ?? 'hello'}</p>
+  const videoElem = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    const video = videoElem.current!
+    if (Hls.isSupported()) {
+      const hls = new Hls({ debug: true })
+      hls.loadSource(source)
+      hls.attachMedia(video)
+      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+        video.muted = true
+        video.play()
+      })
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = source
+      video.addEventListener('canplay', function () {
+        video.play()
+      })
+    }
+  }, [])
+
+  return (
+    <div>
+      <p>{msg ?? 'hello'}</p>
+      <video ref={videoElem} controls autoPlay></video>
+    </div>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url);
+const pathName = require.resolve('@ffmpeg/core');
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
-});
+  assetsInclude: [],
+  plugins: [
+    react(),
+  ],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+})
